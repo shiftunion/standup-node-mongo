874f09709fddf9efcd7acde038930438662497e8
ShiftUnion <nick.cahill@gmail.com>
2016-08-21 13:58:00 +1200

    Completed this demo app

--- controllers/standUp.server.controller.js
+++ controllers/standUp.server.controller.js
@@ -1,40 +1,34 @@
 var StandUp = require('../models/standUp.server.model.js');
 
-exports.list = function (req, res) {
+exports.list = function(req, res) {
     var query = StandUp.find();
-    query.sort({
-        createOn: 'desc'
-    }).limit(12).exec(function (err, results) {
-        res.render('index', {
-            title: 'StandUp - List',
-            notes: results
+
+    query.sort({ createdOn: 'desc'})
+        .limit(12)
+        .exec(function(err, results){
+            res.render('index', {title: 'standUp - List', notes: results});
         });
-    });
+
 };
 
-exports.filterByMember = function (req, res) {
+exports.filterByMember = function(req, res) {
     var query = StandUp.find();
     var filter = req.body.memberName;
 
-    query.sort({
-        createOn: 'desc'
-    });
+    query.sort({ createdOn: 'desc' });
 
-    if (filter.length > 0) {
-        query.where({
-            memberName: filter
-        });
+    if (filter.length > 0)
+    {
+        query.where({ memberName: filter})
     }
 
-    query.exec(function (err, results) {
-        res.render('index', {
-            title: 'StandUp - List',
-            notes: results
-        });
+    query.exec(function(err, results) {
+        res.render('index', { title: 'standUp - List', notes: results });
     });
 };
 
-exports.create = function (req, res) {
+
+exports.create = function(req, res) {
     var entry = new StandUp({
         memberName: req.body.memberName,
         project: req.body.project,
@@ -43,13 +37,20 @@ exports.create = function (req, res) {
         impediment: req.body.impediment
     });
 
-    entry.save();
+    entry.save(function (err) {
+        if (err) {
+            var errMsg = 'Sorry, there was an error saving the stand-up meeting note. ' + err;
+            res.render('newnote', { title: 'standUp - New Note (error)', message: errMsg });
+        }
+        else {
+            console.log('Stand-up meeting note was saved!');
+            // Redirect to the home page to display list of notes...
+            res.redirect(301, '/');
+        }
+    });
 
-    res.redirect(301, '/');
 };
 
 exports.getNote = function (req, res) {
-    res.render('newnote', {
-        title: 'StandUp - New Note'
-    });
-};
+    res.render('newnote', { title: 'standUp - New Note'});
+}
--- models/standUp.server.model.js
+++ models/standUp.server.model.js
@@ -1,14 +1,44 @@
-var mongoose = require('mongoose');
-var Schema  = mongoose.Schema;
+var mongoose = require( 'mongoose' );
+var Schema = mongoose.Schema;
+
+var memberNameValidator = [
+    function (val) {
+        return (val.length > 0 && val.toLocaleLowerCase() != 'none')
+    },
+    // Custom error text...
+    'Select a valid member name.' ];
+
+var requiredStringValidator = [
+    function (val) {
+        var testVal = val.trim();
+        return (testVal.length > 0)
+    },
+    // Custom error text...
+    '{PATH} cannot be empty' ];
 
 var standUpSchema = new Schema({
-    memberName: String,
-    project: String,
-    workYesterday: String,
-    workToday: String,
-    impediment: String,
-    createdOn: {type: Date, default: Date.now }
+    memberName: {
+        type: String,
+        required: true,
+        validate: memberNameValidator },
+    project: {
+        type: String,
+        required: true,
+        validate: requiredStringValidator },
+    workYesterday: {
+        type: String,
+        required: true,
+        validate: requiredStringValidator },
+    workToday: {
+        type: String,
+        required: true,
+        validate: requiredStringValidator },
+    impediment: {
+        type: String,
+        required: true,
+        default: 'none'},
+    createdOn: { type: Date, default: Date.now }
 });
 
-
-module.exports = mongoose.model('StandUp', standUpSchema);
+// Export model...
+module.exports = mongoose.model( 'StandUp', standUpSchema );
